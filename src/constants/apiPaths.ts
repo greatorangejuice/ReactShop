7dc57efd1f73167d04d96bb5691a997976091cd0
Pavel Snigirev <psnigirev@elinext.com>
2021-11-01 15:07:43 +0300

    feat: update api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -6,7 +6,8 @@ const API_PATHS = {
   import: 'https://qfhb4ne77i.execute-api.eu-west-1.amazonaws.com/dev/',
   // bff: 'https://wy7ppwbbh0.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://d8r3r3d0h3.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  // cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://dev-new-cats-cart-api-v2.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
